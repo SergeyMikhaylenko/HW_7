f60020de1b256037734cdb6b352cba85c597a7ae
Sergey_Mikhaylenko <Slad95@mail.ru>
2022-07-16 00:39:51 +0400

    task_47

---
+++ task_47/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 47: Задайте двумерный массив
+// размером m×n, заполненный случайными вещественными числами.
+
+int rows = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[rows, columns];
+Random RealNumber = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+         matrix[i, j] = RealNumber.Next(-26, 26) / 10.0 ;
+
+        Console.Write(matrix[i, j] +"\t" );
+    }
+    Console.WriteLine();
+}
